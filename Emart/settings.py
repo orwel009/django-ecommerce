2d2dbaa0687730aac058fee5605935ffa99c079e
orwel009 <orwelvincent1@gmail.com>
2022-07-25 15:57:07 +0530

    first commit

--- Emart/settings.py
+++ Emart/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,11 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/static'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
@@ -150,3 +152,7 @@ EMAIL_HOST_PASSWORD = 'kuvacljkyslshfky'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Activate Djanngo-Heroku
+django_heroku.settings(locals())
